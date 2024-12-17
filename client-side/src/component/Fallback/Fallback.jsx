a395cc3f0106612b17663fe222e46cd5be950765
suhail th <suhailth17756@gmail.com>
2024-12-17 10:01:22 +0530

    change the fallback sytle

--- client-side/src/component/Fallback/Fallback.jsx
+++ client-side/src/component/Fallback/Fallback.jsx
@@ -5,8 +5,9 @@ function Fallback() {
     <>
     <div className="flex items-center justify-center h-screen " style={{backgroundColor:"black"}}>
     <div className="relative">
-        <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-red-500 animate-spin">
+
+        <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200" ></div>
+        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-red-600 animate-spin">
         </div>
     </div>
 </div>
