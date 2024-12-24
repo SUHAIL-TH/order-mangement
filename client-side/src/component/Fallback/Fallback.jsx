56e690feec097b5dfaf83d14122caae2eb3e9e10
suhail th <suhailth17756@gmail.com>
2024-12-24 11:30:38 +0530

    changed

--- client-side/src/component/Fallback/Fallback.jsx
+++ client-side/src/component/Fallback/Fallback.jsx
@@ -5,7 +5,6 @@ function Fallback() {
     <>
     <div className="flex items-center justify-center h-screen " style={{backgroundColor:"black"}}>
     <div className="relative">
-
         <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200" ></div>
         <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-red-600 animate-spin">
         </div>
